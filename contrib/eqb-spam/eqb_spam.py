88b75bbcb2ab9d4147e4078b7a58126e9a8dea0b
Steve Darveau <steve@darvs.net>
2019-01-25 22:10:49 -0500

    - Added darvs' address

--- contrib/eqb-spam/eqb_spam.py
+++ contrib/eqb-spam/eqb_spam.py
@@ -25,6 +25,7 @@ addresses = [
     "eqbtestnet1q5rvfa3rnx9dykk3cg35fzq86aczh3ed4y0l8vu", # @pete/peteminer-1
     "eqbtestnet1qrucy65lehr67pjeg88gmse7k055la77j8dysmv", # @pete/peteminer-2
     "eqbtestnet1q7qsm0hn5sw456kf34mu3hhcy5lzcy5jfm29rda", # @pete/peteminer-3
+    "eqbtestnet1qzmje7kkk6d2hdmxwhfz0ypqxt69jcfl3w5wxrp", # @darvs/jupiter-mining-1
     ]
 
 MAX_RETRIES = 36
